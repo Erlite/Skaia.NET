73b5e0fb28ad6f4b2cf002b5230137003b1351eb
Erlite <s45405914@sccb.ac.uk>
2018-03-28 21:56:40 +0100

    Added WIP  ConnectionManager

---
+++ SkaiaLib/Core/ConnectionManager.cs
@@ -0,0 +1,19 @@
+﻿
+// ----------------------------------------------------------------------
+// Copyright (c) 2018 All Rights Reserved
+// Author: Younes Meziane
+// Purpose: Handle keeping endpoints alive as pseudo-connections for UDP.
+// ----------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Net;
+
+namespace SkaiaLib.Core
+{
+    public static class ConnectionManager
+    {
+        public static Dictionary<byte, IPEndPoint> Connections { get; } = new Dictionary<byte, IPEndPoint>();
+
+
+    }
+}
