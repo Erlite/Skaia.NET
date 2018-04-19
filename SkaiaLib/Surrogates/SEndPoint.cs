3fe52f01a52f9f742be88d35d4b5615e7811a6ad
Erlite <s45405914@sccb.ac.uk>
2018-04-19 19:04:57 +0100

    Added IPEndPoint surrogate.

---
+++ SkaiaLib/Surrogates/SEndPoint.cs
@@ -0,0 +1,35 @@
+﻿
+// --------------------------------------------
+// Copyright (c) 2018 All Rights Reserved
+// Author: Younes Meziane
+// Purpose: Provide a lightweight IP/Port pair.
+// --------------------------------------------
+
+using System.Net;
+using System.Runtime.InteropServices;
+
+namespace Skaia.Surrogates
+{
+    [StructLayout(LayoutKind.Explicit, Pack = 1)]
+    public struct SEndPoint
+    {
+        [FieldOffset(0)]
+        private byte[] IPv4;
+        [FieldOffset(4)]
+        private int Port;
+
+        public static implicit operator IPEndPoint(SEndPoint value)
+        {
+            return new IPEndPoint(new IPAddress(value.IPv4), value.Port);
+        }
+
+        public static implicit operator SEndPoint(IPEndPoint value)
+        {
+            return new SEndPoint
+            {
+                IPv4 = value.Address.GetAddressBytes(),
+                Port = value.Port
+            };
+        }
+    }
+}
