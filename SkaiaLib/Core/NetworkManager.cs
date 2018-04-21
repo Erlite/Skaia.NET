30b5381f6200d81d2bf1ba08e28e477fd79386db
Erlite <s45405914@sccb.ac.uk>
2018-04-21 13:02:59 +0100

    Refactored NetworkManager

--- SkaiaLib/Core/NetworkManager.cs
+++ SkaiaLib/Core/NetworkManager.cs
@@ -5,12 +5,12 @@
 // Purpose: Handle receiving/sending packets and keeping virtual connections alive.
 // ---------------------------------------------------------------------------
 
-using Skaia.Logging;
+using Skaia.Events;
 using Skaia.Sockets;
+using Skaia.Surrogates;
 using Skaia.Utils;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Threading;
 
@@ -21,25 +21,22 @@ namespace Skaia.Core
     /// </summary>
     public static class NetworkManager
     {
-        private static BaseSocket CoreSocket { get; set; }
+        private static BaseSocket coreSocket;
         private static Node localNode;
+        private static NetworkMode netMode;
+        private static Thread socketThread;
 
+        #region Public API
         /// <summary>
         /// List of nodes on the current network.
         /// </summary>
         // TODO: Make a custom container for the Node list to allow checking for stupid things like two nodes with the same ID.
         public static List<Node> Nodes { get; private set; } = new List<Node>();
+
         /// <summary>
         /// The dispatcher that handles routing network events into their respective callbacks.
         /// </summary>
-        // TODO: Change this to a Dispatcher<NetEvent>
-        public static Dispatcher<Packet> Dispatcher { get; private set; } = new Dispatcher<Packet>();
-
-        /// <summary>
-        /// The thread on which the socket runs.
-        /// </summary>
-        // TODO: Might be a good idea to make this internal or private?
-        public static Thread SocketThread { get; private set; }
+        public static Dispatcher<NetEvent> Dispatcher { get; private set; } = new Dispatcher<NetEvent>();
 
         /// <summary>
         /// Returns the current state of the NetworkManager.
@@ -63,52 +60,65 @@ namespace Skaia.Core
             private set { localNode = value; }
         }
 
+        /// <summary>
+        /// Returns true if the local machine is in Server mode.
+        /// </summary>
+        public static bool IsServer
+        {
+            get
+            {
+                if (!IsStarted)
+                    throw new InvalidOperationException("Cannot get IsServer when the NetworkManager isn't started.");
+                return netMode == NetworkMode.Server;
+            }
+        }
 
-        // TODO: Refactor this to have server/client methods.
-        // TODO: And local node.
-#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
-        public static void Start(int port) => Start(new UDPSocket(), port);
-        public static void Start(BaseSocket socket, int port)
+        /// <summary>
+        /// Returns true if the local machine is in Client mode.
+        /// </summary>
+        public static bool IsClient
+        {
+            get
+            {
+                if (!IsStarted)
+                    throw new InvalidOperationException("Cannot get IsClient when the NetworkManager isn't started.");
+                return netMode == NetworkMode.Client;
+            }
+        }
+        #endregion Public API
+
+        #region Public Methods
+        /// <summary>
+        /// Starts the NetworkManager.
+        /// </summary>
+        /// <param name="socket"> The socket to use. </param>
+        /// <param name="port"> The port to bind to. Will default to a random open port for clients. </param>
+        /// <param name="mode"> The network mode to use. </param>
+        public static void StartNetwork(BaseSocket socket, int port, NetworkMode mode)
         {
             if (IsStarted)
             {
-                SkaiaLogger.LogMessage(MessageType.Error, "Cannot attempt to start ConnectionManager: already started.");
-                return;
+                throw new InvalidOperationException("Cannot start NetworkManager: already started.");
             }
 
-            CoreSocket = socket;
-            IPAddress localAddress = NetUtils.GetLocalEndpoint();
-            IPEndPoint localEndPoint = new IPEndPoint(localAddress, port);
+            coreSocket = socket;
+            IPAddress address = NetUtils.GetLocalEndpoint();
+            port = mode == NetworkMode.Server ? port : 0;
 
-            CoreSocket.BindSocket(localEndPoint);
-            NetUtils.SetConnReset(CoreSocket.Socket);
+            SEndPoint local = SEndPoint.Create(address.GetAddressBytes(), port);
+            coreSocket.BindSocket(local);
+            NetUtils.SetConnReset(coreSocket.Socket);
 
-            SocketThread = new Thread(CoreSocket.Loop)
+            socketThread = new Thread(coreSocket.Loop)
             {
-                Name = "Skaia Socket Thread",
+                Name = "Skaia.NET NetworkManager",
                 IsBackground = true
             };
-            SocketThread.Start();
+
+            socketThread.Start();
+            netMode = mode;
             IsStarted = true;
         }
-
-        public static void Stop()
-        {
-            if (!IsStarted)
-            {
-                SkaiaLogger.LogMessage(MessageType.Error, "Cannot stop NetworkManager: not started.", new InvalidOperationException("Cannot stop server when it isn't started."));
-                return;
-            }
-
-            SkaiaLogger.LogMessage(MessageType.Info, "Stopping NetworkManager...");
-            // TODO: Tell all clients the server is closing.
-            CoreSocket.Socket.Close();
-            Nodes = new List<Node>();
-            SocketThread.Abort();
-
-            SkaiaLogger.LogMessage(MessageType.Info, "NetworkManager succesfully stopped.");
-            IsStarted = false;
-        }
-#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+        #endregion Public Methods
     }
 }
