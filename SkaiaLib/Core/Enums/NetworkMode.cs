75751493a33ea02c6d34fa42ca6a43057902e397
Erlite <s45405914@sccb.ac.uk>
2018-04-21 13:03:05 +0100

    Added NetworkMode

---
+++ SkaiaLib/Core/Enums/NetworkMode.cs
@@ -0,0 +1,17 @@
+﻿namespace Skaia.Core
+{
+    /// <summary>
+    /// Contains the possible network modes that SkaiaLib currently supports.
+    /// </summary>
+    public enum NetworkMode
+    {
+        /// <summary>
+        /// A client node which can connect to a server.
+        /// </summary>
+        Client = 1,
+        /// <summary>
+        /// A server node which can accept incoming connections.
+        /// </summary>
+        Server = 2
+    }
+}
